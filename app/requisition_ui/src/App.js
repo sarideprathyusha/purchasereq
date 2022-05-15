4fda4190cdb53769dd7f1246f06fb4051bdfdf74
sarideprathyusha <98507853+sarideprathyusha@users.noreply.github.com>
2022-05-15 20:45:19 +0530

    Update App.js

--- app/requisition_ui/src/App.js
+++ app/requisition_ui/src/App.js
@@ -4,10 +4,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 import NotFound from "pages/NotFound";
 import AppBar from "components/AppBar";
-import {NavLink} from "react-router-dom"
 import MasterPage from "pages/MasterPage";
 import Create from "pages/Create";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 
@@ -17,16 +15,17 @@ const App = () => {
     const [createForm, setcreateForm] = useState(null);
     const [homeSel, setHomeSel]=useState(true);
     const [createSel, setCreateSel]=useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
 
 const onHomePage = async () => {
-   
+    // const { data } = await instance.get("/Books/$count");
     setHomeSel(true);
     setCreateSel(false);
    
     setcreateForm(null);
   };
   const onCreatePage = async () => {
-  
+    // const { data } = await instance.get("/Books/$count");
     setHomeSel(false);
     setCreateSel(true); 
     setcreateForm(true);
@@ -55,48 +54,62 @@ const onHomePage = async () => {
 
 
 };
+
 const handleEditFormSubmit = (event)=>{
-      event.preventDefault();
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ vendor_id:editFormData.vendor_id,
-        req_name:editFormData.req_name,
-        pur_grp:editFormData.pur_grp,
-        exp_date:editFormData.exp_date,
-        mat_id:editFormData.mat_id,
-        mat_grp:editFormData.mat_grp,
-        qty:editFormData.qty,
-        uom:editFormData.uom,
-        price:editFormData.price,
-        cur:editFormData.cur,
-        plant:editFormData.plant,
-        approved: false })
-    };
-  
-    fetch('purchasereq_destination_srv/Requests', requestOptions).then(response => response.json());
     
-  
-      console.log(editFormData);
-      setEditFormData(
-        {
-            "vendor_id":"",
-            "req_name":"",
-            "pur_grp":"",
-            "exp_date":"",
-            "mat_id":"",
-            "mat_grp":"",
-            "qty":"",
-            "uom":"",
-            "price":"",
-            "cur":"",
-            "plant":"",
-            "ven_name":"",
-            "mat_desc":"",
-            "approved": false 
-         }
-      );
-  
+      event.preventDefault();
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ vendor_id:editFormData.vendor_id,
+            req_name:editFormData.req_name,
+            pur_grp:editFormData.pur_grp,
+            exp_date:editFormData.exp_date,
+            mat_id:editFormData.mat_id,
+            mat_grp:editFormData.mat_grp,
+            qty:editFormData.qty,
+            uom:editFormData.uom,
+            price:editFormData.price,
+            cur:editFormData.cur,
+            plant:editFormData.plant,
+            approved: false })
+    
+        };
+      
+        fetch('bookshop_destination_srv/Requests', requestOptions).then(response => response.json());
+        // instance.get("/functionCall(param='"+ query +"')"); 
+      
+          console.log(editFormData);
+          setEditFormData(
+            {
+                "vendor_id":"",
+                "req_name":"",
+                "pur_grp":"",
+                "exp_date":"",
+                "mat_id":"",
+                "mat_grp":"",
+                "qty":"",
+                "uom":"",
+                "price":"",
+                "cur":"",
+                "plant":"",
+                "ven_name":"",
+                "mat_desc":"",
+                "approved": false 
+             }
+          );
+          setHomeSel(true);
+          setCreateSel(false);
+         
+          setcreateForm(null);
+        //   setEditItemId(null);
+        //   loadData(true);
+    
+      
+      
+      
+
   };
    
 
@@ -110,32 +123,9 @@ const handleEditFormSubmit = (event)=>{
         <div className="collapse navbar-collapse">
         
         <h3 style={{marginLeft:"1rem"}}>Purchase Requisition</h3>
-        <Fragment>
-            {homeSel ?(
-            <a style={{marginLeft:"2rem",color:"#78859f"}}>
-            <span onClick={onHomePage} >Request Status</span>
-            </a>
-            ):(
-                <a style={{marginLeft:"2rem", color:"white"}}>
-                <span onClick={onHomePage} >Request Status</span>
-                </a>
-            )}
-            </Fragment>
+        
+            <button type="submit" style={{borderColor:'white',color: 'white',marginLeft:'50rem',backgroundColor: '#354a5f'}} onClick={onCreatePage}>+ Create Request</button>
             
-               <Fragment>
-            {createSel ?(
-            <a style={{marginLeft:"2rem",color:"#78859f"}}>
-              <span onClick={onCreatePage} >Create</span>
-              </a>
-            ):(
-               <a style={{marginLeft:"2rem",color:"white"}}>
-              <span onClick={onCreatePage} >Create</span>
-              </a>
-            )}
-            </Fragment>
-              
-             
-          
         </div>
       </div>
     </nav>
@@ -143,16 +133,21 @@ const handleEditFormSubmit = (event)=>{
     <div>
  
     <form onSubmit={handleEditFormSubmit}>
-    <Fragment>
-                {createForm ? (
-                     <Create editFormData={editFormData} handleEditFormChange={handleEditFormChange}/>
-                     ): ( 
-                     <MasterPage/>) }
-             
+  <Fragment>
+            {createForm ?
+            (<Create editFormData={editFormData} handleEditFormChange={handleEditFormChange}/>):(<MasterPage/>)
+            }
+
+
+            {/* <MasterPage/> */}
             </Fragment>
+            
+           
 
     </form>
     </div>
+
+   
             
     </div>
    
@@ -162,3 +157,4 @@ const handleEditFormSubmit = (event)=>{
 };
 
 export default App;
+
